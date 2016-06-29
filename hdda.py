f9edf71c8a5656fa261c4e6dcdc586dcafd1b0c4
mfauvel <mathieu.fauvel@ensat.fr>
2016-06-29 17:47:21 +0200

    Move MDA class to a new file
    Move import definitio etc ... to mda.py

--- hdda.py
+++ hdda.py
@@ -1,30 +1,14 @@
 # -*- coding: utf-8 -*-
 import scipy as sp
 from scipy import linalg
-from sklearn.cross_validation import StratifiedKFold
 from sklearn.cluster import KMeans
-from sklearn.metrics import confusion_matrix
 import accuracy_index as ai
-import multiprocessing
-from joblib import Parallel, delayed
 
 ## Numerical precision - Some constant
 EPS = sp.finfo(sp.float64).eps
 MIN = sp.finfo(sp.float64).min
 MAX = sp.finfo(sp.float64).max
 E_MAX = sp.log(MAX) # Maximum value that is possible to compute with sp.exp
-
-## Worker function for MDA
-def workerMda(x,MODEL,th,C):
-    """
-    """
-    model = HDGMM()
-    model.bic = MIN
-    iter = 0
-    while model.bic == MIN:
-        model.fit_all(x,MODEL=MODEL,th=th,C=C,random_state=iter)
-        iter+=1
-    return model
 
 ## HDDA Class
 class HDGMM():
@@ -475,83 +459,3 @@ class HDGMM():
         self.model = MODEL[t[0][0]]
         self.fit(x,param=param)
 
-
-class MDA():
-    def __init__(self):
-        """
-        """
-        self.model = []
-        self.prop = []
-        self.j = []
-
-    def fit(self,x,y,MODEL=['M1','M2','M3','M4','M5','M6','M7','M8'],th=[0.1], C = [1,2,3,4,5,6]):
-        """
-        """
-        n,d = x.shape
-        nbC = int(y.max())
-
-        # Get the indices of samples for each class
-        self.j = [sp.where(y==(c_+1))[0] for c_ in xrange(nbC)]
-
-        # Get the proportion of each class
-        self.prop = [float(j_.size)/n for j_ in self.j]
-
-        # Learn each class mixture
-        self.model = Parallel(n_jobs=4)(delayed(workerMda)(x[j_,:],MODEL,th,C) for j_ in self.j) # TODO: tester si cela n'a pas convergé -> il faut relancer !!
-
-        
-
-    def predict(self,xt):
-        """
-        """
-        # Initialization of the output
-        nt,C = xt.shape[0],len(self.prop)
-        P = sp.empty((nt,C))
-
-        for c in xrange(C):
-        # Compute the posterior
-            K = self.model[c].predict(xt,out='ki')
-            K *= (-0.5)
-            K[K>E_MAX] = E_MAX
-            sp.exp(K,out=K)
-            P[:,c]=self.prop[c]*K.sum(axis=1)
-        
-        # Get the class with the largest probability
-        yp = sp.argmax(P,1)+1
-        return yp
-    
-    def cross_validation(self,x,y,th,MODEL=['M1','M2','M3','M4','M5','M6','M7','M8'],C=[1,2,3,4,5,6],v=5,random_state=0):
-        """
-        """
-        def get_kappa(confu):
-            """
-                Compute Kappa
-            """
-            n = sp.sum(confu)
-            nl = sp.sum(confu,axis=1)
-            nc = sp.sum(confu,axis=0)
-            OA = sp.sum(sp.diag(confu))/float(n)
-
-            return ((n**2)*OA - sp.sum(nc*nl))/(n**2-sp.sum(nc*nl))
-
-        n,d = x.shape
-        nth = len(th)
-        
-        ## Initialization of the stratified K-fold
-        KF = StratifiedKFold(y.reshape(y.size,),v,random_state=random_state)
-        Kappa = sp.zeros((nth,))
-
-        for train,test in KF:
-            for i,th_ in enumerate(th):# TODO: Change model to mda
-                model = MDA()
-                model.fit(x[train,:],y[train],MODEL=MODEL,th=[th_],C=C)
-                yp = model.predict(x[test,:])
-                confu = confusion_matrix(y[test],yp)
-                Kappa[i] += get_kappa(confu)
-                
-        Kappa/=v
-        ind = sp.argmax(Kappa)
-        self.th  = [th[ind]]
-        self.Kappa = Kappa
-        # Learn model with optimal parameter
-        self.fit(x,y,MODEL=MODEL,th=self.th,C=C)
